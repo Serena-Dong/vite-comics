bfee24b3be0a4514f6797513147fca8800ee2fe5
Serena-Dong <serenadong3159@icloud.com>
2023-01-20 16:39:39 +0100

    data

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <script setup>
-import AppHeader from './components/AppHeader.vue'
-import AppMain from './components/AppMain.vue'
-import AppFooter from './components/AppFooter.vue'
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
 </script>
 
 <template>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,4 +1,63 @@
-<script></script>
+<script>
+export default {
+    data() {
+        return {
+            data: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false,
+                },
+            ]
+        }
+    }
+}
+</script>
 
 <template></template>
 
