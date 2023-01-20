209920bc3e7208766951c26223cfe4f4cc94684c
Serena-Dong <serenadong3159@icloud.com>
2023-01-20 16:12:27 +0100

    base

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@ import AppFooter from './components/AppFooter.vue'
 </script>
 
 <template>
-
+  <AppHeader></AppHeader>
+  <AppMain></AppMain>
+  <AppFooter></AppFooter>
 </template>
 
 <style scoped>
