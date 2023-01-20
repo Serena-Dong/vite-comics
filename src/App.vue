8fbb22eb8f8668a353958df9ac1e9ee599c55f4f
Serena-Dong <serenadong3159@icloud.com>
2023-01-20 16:09:10 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
